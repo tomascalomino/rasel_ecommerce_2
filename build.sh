6ad9bda9ae52596c57f97e8ed76c932538e5ab8e
Tomas Calomino <tomascalomino@gmail.com>
2026-02-20 15:04:05 -0300

    Fix build: use python -m pip and correct collectstatic flag

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
-python backend/manage.py collectstatic --no-input
+python -m pip install -r requirements.txt
+python backend/manage.py collectstatic --noinput
 python backend/manage.py migrate
