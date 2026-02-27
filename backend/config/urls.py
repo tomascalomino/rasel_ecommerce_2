fc0c8d461b43fe1a9311948fdf723c3769b77e6b
Tomas Calomino <tomascalomino@gmail.com>
2026-02-27 01:12:36 -0300

    Fix media serving when DEBUG=False using re_path and serve

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
@@ -31,4 +32,8 @@ urlpatterns = [
 ]
 
 if settings.SERVE_MEDIA:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
